4ce08f068121b2b3fa967e41b459d6536214d863
samvnkauffman-pc <samvnkauffman@gmail.com>
2017-01-25 21:32:09 -0500

    "save"

--- php/movies.php
+++ php/movies.php
@@ -28,6 +28,62 @@ $stmt->execute([
 ]);
 
 
+
+
+
+
+require_once "config.php";
+
+
+class Db {
+	
+	private $title;
+	private $review;
+	private $imdb;
+	private $poster;
+	private $rank;
+	
+	function __construct($title, $review, $imdb, $poster, $rank) {
+		$this->$title = $title ?? "";
+		$this->$review = $review ?? "";
+		$this->$imdb = $imdb ?? "";
+		$this->$poster = $poster ?? "";
+		$this->$rank = $rank ?? "";
+	}
+
+	//connect to database
+	private $db = new PDO($dsn, $username, $password);
+
+	//disable emulation of prepared statements (for compatibility)
+	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	//error handling
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+	public function createMovie($title, $review, $imdb, $poster, $rank) {
+		$stmt = $db->prepare(
+			"INSERT INTO movies(title, review, imdb, poster, rank)
+			VALUES (:title, :review, :imdb, :poster, :rank)"
+		);
+
+		$stmt->execute([
+			":title" => $title,
+			":review" => $review,
+			":imdb" => $imdb,
+			":poster" => $poster,
+			":rank" => $rank
+		]);
+	}
+	
+	public function foreachMovie($message) {
+		foreach($db->query("SELECT * FROM movies") as $movie) {
+			echo $message;
+		}
+	}
+}
+
+
+
+
 //add title and review to database (create)
 //also add all other imdb details to database? (or should these be dynamically generated)
 
